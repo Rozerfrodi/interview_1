b7658f01980b77b77664bad75a43f489af31ade6
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-04-06 23:42:11 +0300

    added checking file exists

--- services/parser.py
+++ services/parser.py
@@ -1,10 +1,12 @@
 import csv
-
+import os
 
 def load_data(files: list[str]) -> list[dict]:
     data = []
 
     for file in files:
+        if not os.path.exists(file):
+            raise FileNotFoundError('File does not exist')
         with open(file, newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f)
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,10 +6,10 @@ def test_parse_args(monkeypatch):
     monkeypatch.setattr(
         sys,
         "argv",
-        ["main.py", "--files", "a.csv", "--report", "median-coffee"]
+        ["main.py", "--files", "asada.csv", "--report", "median-coffee"]
     )
 
     args = parse_args()
 
     assert args.report == "median-coffee"
-    assert args.files == ["a.csv"]
+    assert args.files == ["asada.csv"]
